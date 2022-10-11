341be42055712386d9c475e9bb187f08e947c01a
sanudas2018 <sanu.das08103@gmail.com>
2022-10-11 17:29:09 +0600

    Header banar Section done with responsive

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,33 @@
 import React from "react";
+
 import "./Home.css";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <section id="banner" className=" py-4">
+        <div className="container mx-auto py-2 px-2 flex md:flex-row-reverse bg-indigo-200 rounded-xl">
+          {/* ========  */}
+          <div className="md:w-1/2 flex flex-col align-middle justify-center textContent">
+            <div className="ml-4 ">
+              <h1 className=" text-cyan-800 md:font-semibold text-xl">
+                This website is tested by your programming learning basement
+                MCQ.These questions will make your learning more complete. SO
+                LETS GO.
+              </h1>
+            </div>
+          </div>
+          <div className="flex md:flex-col md:justify-center md:w-1/2 imageBox">
+            <div className="">
+              <img
+                className="w-[100%] h-[20vh] rounded-[10px] imgBoxInner"
+                alt="img"
+                src="https://res.cloudinary.com/dwac7hlfp/image/upload/v1665485264/edulerning_om0vbe.png"
+              ></img>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
